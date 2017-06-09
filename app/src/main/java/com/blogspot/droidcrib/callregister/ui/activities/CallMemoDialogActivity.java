3a0c08d41674ef05e0af1805886d159005f0c780
Andrey Bulanov <droidcrib@gmail.com>
2017-06-09 10:02:52 +0300

    Picker activity setup done

--- app/src/main/java/com/blogspot/droidcrib/callregister/ui/activities/CallMemoDialogActivity.java
+++ app/src/main/java/com/blogspot/droidcrib/callregister/ui/activities/CallMemoDialogActivity.java
@@ -1,30 +1,22 @@
 package com.blogspot.droidcrib.callregister.ui.activities;
 
 import android.Manifest;
-import android.app.Activity;
-import android.content.Context;
 import android.content.DialogInterface;
 import android.content.Intent;
 import android.content.pm.PackageManager;
 import android.graphics.Bitmap;
-import android.graphics.Rect;
 import android.os.Bundle;
-import android.support.design.widget.TabLayout;
 import android.support.v4.app.ActivityCompat;
 import android.support.v4.content.ContextCompat;
-import android.support.v4.view.ViewPager;
 import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.text.Editable;
 import android.text.TextWatcher;
 import android.util.Log;
 import android.view.View;
-import android.view.ViewTreeObserver;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ImageView;
-import android.widget.LinearLayout;
-import android.widget.RelativeLayout;
 import android.widget.TextView;
 
 import com.blogspot.droidcrib.callregister.R;
@@ -33,13 +25,12 @@ import com.blogspot.droidcrib.callregister.eventbus.NewCallEvent;
 import com.blogspot.droidcrib.callregister.model.CallRecord;
 import com.blogspot.droidcrib.callregister.model.ContactCard;
 import com.blogspot.droidcrib.callregister.telephony.ContactsProvider;
-import com.blogspot.droidcrib.callregister.ui.adapters.MeasuredViewPager;
-import com.blogspot.droidcrib.callregister.ui.adapters.TabsPagerAdapter;
 
 import org.greenrobot.eventbus.EventBus;
 
-import java.text.SimpleDateFormat;
 import java.util.Date;
+
+import static com.blogspot.droidcrib.callregister.contract.Constants.EXTRA_RECORD_ID;
 
 public class CallMemoDialogActivity extends AppCompatActivity {
 
@@ -58,10 +49,7 @@ public class CallMemoDialogActivity extends AppCompatActivity {
     private Button mCancelButton;
     private Bitmap mAvatarBitmap;
     private String mAvatarUri;
-    private MeasuredViewPager mViewPager;
-    private LinearLayout mRootLinear;
-    private LinearLayout mButtonsHolder;
-    private RelativeLayout mPickerMainLayout;
+
 
 
     private static final String TAG = "CallMemoDialogActivity";
@@ -77,54 +65,7 @@ public class CallMemoDialogActivity extends AppCompatActivity {
 
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_request_action);
-
         this.setFinishOnTouchOutside(false);
-
-
-        //
-        //  Setup TabLayout
-        //
-        TabLayout tabLayout = (TabLayout) findViewById(R.id.tab_layout);
-        tabLayout.addTab(tabLayout.newTab().setText("Date"));
-        tabLayout.addTab(tabLayout.newTab().setText("Time"));
-        tabLayout.addTab(tabLayout.newTab().setText("Memo"));
-        tabLayout.setTabGravity(TabLayout.GRAVITY_FILL);
-
-        //
-        //  Setup ViewPager
-        //
-        mViewPager = (MeasuredViewPager) findViewById(R.id.pager);
-        final TabsPagerAdapter adapter = new TabsPagerAdapter(this, tabLayout.getTabCount());
-        mViewPager.setAdapter(adapter);
-
-        mViewPager.addOnPageChangeListener(new TabLayout.TabLayoutOnPageChangeListener(tabLayout) {
-            @Override
-            public void onPageSelected(int position) {
-                super.onPageSelected(position);
-                if (position == 0) {
-                    // do something with content
-                }
-                if (position == 1) {
-                    // do something with content
-                }
-            }
-        });
-
-        tabLayout.setOnTabSelectedListener(new TabLayout.OnTabSelectedListener() {
-            @Override
-            public void onTabSelected(TabLayout.Tab tab) {
-                mViewPager.setCurrentItem(tab.getPosition());
-            }
-
-            @Override
-            public void onTabUnselected(TabLayout.Tab tab) {
-            }
-
-            @Override
-            public void onTabReselected(TabLayout.Tab tab) {
-            }
-        });
-
 
         // Get call data
         mPhoneNumber = getIntent().getStringExtra(Constants.EXTRA_PHONE_NUMBER);
@@ -140,20 +81,14 @@ public class CallMemoDialogActivity extends AppCompatActivity {
         mRecordId = CallRecord.insert(mContactName, mPhoneNumber, mAvatarUri, mCallType, mCallDate);
         EventBus.getDefault().post(new NewCallEvent());
 
-        mDisplayName = (TextView) findViewById(R.id.id_dialog_person_name);
-        mDisplayCallType = (ImageView) findViewById(R.id.id_dialog_call_type);
-        mDisplayAvatar = (ImageView) findViewById(R.id.id_dialog_avatar);
+        mDisplayName = (TextView) findViewById(R.id.id_person_name);
+        mDisplayCallType = (ImageView) findViewById(R.id.id_call_type);
+        mDisplayAvatar = (ImageView) findViewById(R.id.id_avatar);
         mNoteButton = (Button) findViewById(R.id.id_dialog_button_note);
         mReminderButton = (Button) findViewById(R.id.id_dialog_button_reminder);
         mCancelButton = (Button) findViewById(R.id.id_dialog_button_cancel);
         mNote = (EditText) findViewById(R.id.id_dialog_note);
-        mRootLinear = (LinearLayout) findViewById(R.id.root_linear);
-        mButtonsHolder = (LinearLayout) findViewById(R.id.buttons_holder);
-        mPickerMainLayout = (RelativeLayout) findViewById(R.id.picker_main_layout);
 
-        if (flag){
-            mPickerMainLayout.setVisibility(View.VISIBLE);
-        }
 
         // Setup views
         mDisplayName.setText(mContactName);
@@ -181,7 +116,6 @@ public class CallMemoDialogActivity extends AppCompatActivity {
             @Override
             public void onClick(View v) {
                 mNote.setVisibility(View.VISIBLE);
-                mPickerMainLayout.setVisibility(View.GONE);
             }
         });
 
@@ -192,12 +126,9 @@ public class CallMemoDialogActivity extends AppCompatActivity {
                 // TODO: start another activity with almost same layout to handle big datepicker
                 // TODO: replace buttons at the bottom with FAB "Done"
                 long dateMilliseconds = mCallDate.getTime();
-                Intent intent = new Intent(CallMemoDialogActivity.this, CallMemoDialogActivity.class);
+                Intent intent = new Intent(CallMemoDialogActivity.this, NewReminderActivity.class);
                 //intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
-                intent.putExtra(Constants.EXTRA_PHONE_NUMBER, mPhoneNumber);
-                intent.putExtra(Constants.EXTRA_DATE, dateMilliseconds);
-                intent.putExtra(Constants.EXTRA_CALL_TYPE, mCallType);
-                intent.putExtra("qaz", true);
+                intent.putExtra(EXTRA_RECORD_ID, mRecordId);
                 CallMemoDialogActivity.this.startActivity(intent);
                 finish();
 
--- app/src/main/java/com/blogspot/droidcrib/callregister/ui/activities/NewReminderActivity.java
+++ app/src/main/java/com/blogspot/droidcrib/callregister/ui/activities/NewReminderActivity.java
@@ -1,18 +1,128 @@
 package com.blogspot.droidcrib.callregister.ui.activities;
 
+import android.graphics.Bitmap;
+import android.net.Uri;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.design.widget.TabLayout;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import com.blogspot.droidcrib.callregister.R;
+import com.blogspot.droidcrib.callregister.contract.Constants;
+import com.blogspot.droidcrib.callregister.model.CallRecord;
+import com.blogspot.droidcrib.callregister.ui.adapters.MeasuredViewPager;
+import com.blogspot.droidcrib.callregister.ui.adapters.TabsPagerAdapter;
+
+import java.util.Date;
+
+import static com.blogspot.droidcrib.callregister.contract.Constants.EXTRA_RECORD_ID;
 
 /**
- * Created by BulanovA on 09.06.2017.
+ *
  */
 
-public class NewReminderActivity  extends AppCompatActivity  {
+public class NewReminderActivity extends AppCompatActivity {
+
+    private MeasuredViewPager mViewPager;
+    private long mRecordId;
+    private TextView mDisplayName;
+    private EditText mNote;
+    private ImageView mDisplayCallType;
+    private ImageView mDisplayAvatar;
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_new_reminder);
+
+        mRecordId = getIntent().getLongExtra(EXTRA_RECORD_ID, 0);
+        CallRecord callRecord = CallRecord.getRecordById(mRecordId);
+
+        mDisplayName = (TextView) findViewById(R.id.id_person_name);
+        mDisplayCallType = (ImageView) findViewById(R.id.id_call_type);
+        mDisplayAvatar = (ImageView) findViewById(R.id.id_avatar);
+        mNote = (EditText) findViewById(R.id.id_dialog_note);
+
+
+        // Name
+        mDisplayName.setText(callRecord.name);
+        // Avatar
+        if (callRecord.avatarUri != null) {
+            mDisplayAvatar.setImageURI(Uri.parse(callRecord.avatarUri));
+        } else {
+            mDisplayAvatar.setImageResource(R.drawable.ic_person_black_48dp);
+        }
+        // Call type
+        switch (callRecord.callType) {
+            case Constants.INCOMING_CALL:
+                mDisplayCallType.setImageResource(R.drawable.ic_call_received_black_48dp);
+                break;
+
+            case Constants.OUTGOING_CALL:
+                mDisplayCallType.setImageResource(R.drawable.ic_call_made_black_48dp);
+                break;
+
+            case Constants.MISSED_CALL:
+                mDisplayCallType.setImageResource(R.drawable.ic_call_missed_black_48dp);
+                break;
+        }
+
+
+        //  Setup TabLayout
+        TabLayout tabLayout = (TabLayout) findViewById(R.id.tab_layout);
+        tabLayout.addTab(tabLayout.newTab().setText("Date"));
+        tabLayout.addTab(tabLayout.newTab().setText("Time"));
+        tabLayout.addTab(tabLayout.newTab().setText("Memo"));
+        tabLayout.setTabGravity(TabLayout.GRAVITY_FILL);
+
+        //  Setup ViewPager
+        mViewPager = (MeasuredViewPager) findViewById(R.id.pager);
+        final TabsPagerAdapter adapter = new TabsPagerAdapter(this, tabLayout.getTabCount());
+        mViewPager.setAdapter(adapter);
+        mViewPager.addOnPageChangeListener(new TabLayout.TabLayoutOnPageChangeListener(tabLayout) {
+            @Override
+            public void onPageSelected(int position) {
+                super.onPageSelected(position);
+                if (position == 0) {
+                    // do something with content
+                }
+                if (position == 1) {
+                    // do something with content
+                }
+            }
+        });
+
+        tabLayout.setOnTabSelectedListener(new TabLayout.OnTabSelectedListener() {
+            @Override
+            public void onTabSelected(TabLayout.Tab tab) {
+                mViewPager.setCurrentItem(tab.getPosition());
+            }
+
+            @Override
+            public void onTabUnselected(TabLayout.Tab tab) {
+            }
+
+            @Override
+            public void onTabReselected(TabLayout.Tab tab) {
+            }
+        });
+
+
+        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                        .setAction("Action", null).show();
+            }
+        });
 
 
     }
