a45f297a4fcbec8e3878543f796c3aa02db40989
Andrey Bulanov <droidcrib@gmail.com>
2017-06-29 10:31:04 +0300

    prod ver 1

--- app/src/main/java/com/blogspot/droidcrib/callregister/ui/activities/SingleFragmentActivity.java
+++ app/src/main/java/com/blogspot/droidcrib/callregister/ui/activities/SingleFragmentActivity.java
@@ -1,33 +1,18 @@
 package com.blogspot.droidcrib.callregister.ui.activities;
 
 
-import android.Manifest;
-import android.content.Context;
-import android.content.DialogInterface;
-import android.content.pm.PackageManager;
 import android.os.Bundle;
-import android.os.Handler;
 import android.support.annotation.Nullable;
-import android.support.v4.app.ActivityCompat;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentManager;
-import android.support.v4.content.ContextCompat;
 import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.ActionBarDrawerToggle;
-import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
-import android.telephony.TelephonyManager;
-import android.util.Log;
-import android.view.View;
-import android.widget.TextView;
-import android.widget.Toast;
 
 import com.blogspot.droidcrib.callregister.R;
 import com.blogspot.droidcrib.callregister.model.AlarmRecord;
-import com.blogspot.droidcrib.callregister.telephony.ContactsProvider;
 import com.blogspot.droidcrib.callregister.ui.fragments.CallDetailsFragment;
-import com.blogspot.droidcrib.callregister.ui.fragments.CallsListFragment;
 
 import static com.blogspot.droidcrib.callregister.contract.Constants.ACTION_SHOW_ALARM_DETAILS;
 import static com.blogspot.droidcrib.callregister.contract.Constants.EXTRA_ALARM_RECORD_ID;
